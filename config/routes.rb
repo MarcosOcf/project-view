4e966bbf92e5927ee204795366a416eb76f744af
marcos.ocf01 <marcos.ocf01@gmail.com>
2014-08-20 22:26:38 -0300

    adicionando css e corrigindo links

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'projects/new', to: 'projects#new', as: 'new_project'
   get 'projects/index', to: 'projects#index', as: 'projects'
   get 'projects/:id', to: 'projects#show', as: 'project'
-  post 'projects', to: 'projects#create'
+  post 'projects/index', to: 'projects#create'
   delete 'projects/:id', to: 'projects#destroy'
 
   #-------------- tab ----------------
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
   scope "projects/:project_id" do
     get 'tabs', to: 'tabs#index', as: 'tabs'
     post 'tabs', to: 'tabs#create', as: 'new_tab_to_project'
-
     get 'tabs/new', to: 'tabs#new', as: 'new_tab'
     get 'tabs/:id', to: 'tabs#show', as: 'tab'
     delete 'tabs/:id', to: 'tabs#destroy', as: 'destroy_tab'
